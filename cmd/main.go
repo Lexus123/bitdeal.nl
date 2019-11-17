f79443ecbaa413522c44dcb0adc542d3ae6f5178
Lex van der Werff <>
2019-11-17 15:52:14 +0100

    no message

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	// "bitdeal.nl/etl"
 	"bitdeal.nl/server"
 )
 
 func main() {
-	// go etl.StartRequesting()
 	server.Host()
 }
--- server/server.go
+++ server/server.go
@@ -10,5 +10,9 @@ Host will init the routing and fire up a server
 */
 func Host() {
 	router := NewRouter()
+	fs := http.FileServer(http.Dir("static"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	log.Fatal(http.ListenAndServe(":8003", router))
 }
