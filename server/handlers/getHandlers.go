2923e3ef94f942880961a5a21c987140f654e446
Lex van der Werff <>
2019-11-18 15:08:55 +0100

    no message

--- server/handlers/getHandlers.go
+++ server/handlers/getHandlers.go
@@ -7,9 +7,7 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
-	"sort"
 
-	"bitdeal.nl/etl"
 	"bitdeal.nl/models"
 )
 
@@ -63,50 +61,51 @@ GetPrices ...
 */
 func GetPrices(w http.ResponseWriter, r *http.Request) {
 	// Read JSON body of request and check for errors. For example: buy, eur, 100
-	bodyAsByte, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	// bodyAsByte, err := ioutil.ReadAll(r.Body)
+	// defer r.Body.Close()
+	// if err != nil {
+	// 	http.Error(w, err.Error(), 500)
+	// 	return
+	// }
 
-	// Unmarshal the body and check for errors
-	var message models.GetPricesData
-	err = json.Unmarshal(bodyAsByte, &message)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	// // Unmarshal the body and check for errors
+	// var message models.GetPricesData
+	// err = json.Unmarshal(bodyAsByte, &message)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), 500)
+	// 	return
+	// }
 
-	// Send message to the extractor to get data from brokers
-	dataChannel := make(chan interface{}, 5)
-	defer close(dataChannel)
-	go etl.DoRequests(message, dataChannel)
+	// // Send message to the extractor to get data from brokers
+	// dataChannel := make(chan interface{}, 5)
+	// defer close(dataChannel)
+	// go etl.DoRequests(message, dataChannel)
 
-	getPricesResponse := models.GetPricesResponse{
-		Type:     message.Type,
-		Currency: message.Currency,
-	}
+	// getPricesResponse := models.GetPricesResponse{
+	// 	Type:     message.Type,
+	// 	Currency: message.Currency,
+	// }
 
-	for i := 0; i < 5; i++ {
-		getPricesResponse.ExchangeRates = append(getPricesResponse.ExchangeRates, etl.Transform(message, <-dataChannel))
-	}
+	// for i := 0; i < 5; i++ {
+	// 	getPricesResponse.ExchangeRates = append(getPricesResponse.ExchangeRates, etl.Transform(message, <-dataChannel))
+	// }
 
-	sort.Slice(getPricesResponse.ExchangeRates[:], func(i, j int) bool {
-		return getPricesResponse.ExchangeRates[i].Rate < getPricesResponse.ExchangeRates[j].Rate
-	})
+	// sort.Slice(getPricesResponse.ExchangeRates[:], func(i, j int) bool {
+	// 	return getPricesResponse.ExchangeRates[i].Rate < getPricesResponse.ExchangeRates[j].Rate
+	// })
 
-	getPricesResponse.BestRate = getPricesResponse.ExchangeRates[0].Rate
-	getPricesResponse.MostReviews = 5485
+	// getPricesResponse.BestRate = getPricesResponse.ExchangeRates[0].Rate
+	// getPricesResponse.MostReviews = 5485
 
-	output, err := json.Marshal(getPricesResponse)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	// output, err := json.Marshal(getPricesResponse)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), 500)
+	// 	return
+	// }
 
-	w.Header().Set("content-type", "application/json")
-	w.Write(output)
+	// w.Header().Set("content-type", "application/json")
+	// w.Write(output)
+	fmt.Fprintf(w, "Hi there!")
 }
 
 /*
